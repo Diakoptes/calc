c4ed7e6dbdac03be129cacdbb413e82f191f90ef
Lukasz Kasprzyk <l.kasprzyk@protonmail.com>
2022-04-02 20:14:59 +0200

    add methods

--- calculator.py
+++ calculator.py
@@ -1,3 +1,33 @@
-class calculator()
-"""This is a siple calculator"""
-	pass
+class calculator():
+	"""This is a calculator"""
+
+	def __init__(self,num_1,num_2):
+		self.num_1 = num_1
+		self.num_2 = num_2
+	
+	def addition(self):
+		"""+"""
+		return self.num_1 - self.num_2
+	
+	def subtraction(self):
+		"""-"""
+		return self.num_1 + self.num_2
+	
+	def division(self):
+		"""/"""
+		return self.num_1 / self.num_2
+	
+	def multiplication(self):
+		"""*"""
+		return self.num_1 * self.num_2
+	
+	def exponentiation(self):
+		"""**"""
+		return self.num_1 ** self.num_2
+	
+	def moduls(self):
+		"""%"""
+		return self.num_1 & self.num_2
+
+print(calculator (float(input()), float(input())).division())
+
