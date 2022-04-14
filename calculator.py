f232c09beb9230cc0badddf6be2a77b9b9620d5e
Lukasz Kasprzyk <l.kasprzyk@protonmail.com>
2022-04-14 22:03:25 +0200

    Added layout.py from calculator.py

--- calculator.py
+++ calculator.py
@@ -1,79 +1,60 @@
-from cmath import exp
 import tkinter as tk
 from tkinter import ttk
-from math import sqrt
+from layout import *
 
-root = tk.Tk()
-root.title("calculator")
+class Calculator():
+    """
+    This is simple calculator that can: add, subtract, multiplication,
+    division, exponentiation and give you  rest of divided.
+    Calculator will check your numbers, you cannot divide by zero, 
+    and you can give only numbers.
+    """
 
-result = ttk.Label(text="", style="BW.TLabel")
-result.pack()
+    def calc_sum(first_num, second_num):
+        """Addition function."""
+        
+        try:
+            return float(first_num) + float(second_num)
+        except ValueError as err:
+            return "Unexpected value!"
 
-first_num = ttk.Entry(root, text="input calculator")
-first_num.pack()
-second_num = ttk.Entry(root, text="input calculator1")
-second_num.pack()
+    def calc_sub(first_num, second_num):
+        """Subtract function"""
 
-def calc_sum(first_num, second_num):
+        try:
+            return float(first_num) - float(second_num)
+        except ValueError as err:
+            return "Unexpected value!"
+        
+    def calc_multi(first_num, second_num):
+        """Multiplication function."""
+        
+        try:
+            return float(first_num) * float(second_num)
+        except ValueError as err:
+            return "Unexpected value!"
 
-    try:
-        return float(first_num) + float(second_num)
-    except ValueError as err:
-        return "unexpected value"
+    def calc_div(first_num, second_num):
+        """Division function."""
+        try:
+            return float(first_num) / float(second_num)
+        except ZeroDivisionError as err:
+            result.config(text=err)
+        except ValueError as err:
+            result.config(text="Unexpected value!")
 
-def calc_sub(first_num, second_num):
+    def calc_exp(first_num, second_num):
+        """Exponentiation function"""
+        
+        try:
+            return float(first_num) ** float(second_num)
+        except ValueError as err:
+            result.config(text="Unexpected value!")
 
-    try:
-        return float(first_num) - float(second_num)
-    except ValueError as err:
-        return "unexpected value"
-    
-def calc_multi(first_num, second_num):
-    try:
-       return float(first_num) * float(second_num)
-    except ValueError as err:
-        return "unexpected value"
+    def calc_mod(first_num, second_num):
+        """Remainder of divided function."""
 
-def calc_div(first_num, second_num):
-
-    try:
-        return float(first_num) / float(second_num)
-    except ZeroDivisionError as err:
-        result.config(text=err)
-    except ValueError as err:
-        result.config(text="ValueError")
-
-def calc_exp(first_num, second_num):
-
-    try:
-        return float(first_num) ** float(second_num)
-    except ValueError as err:
-        result.config(text="ValueError")
-
-def calc_mod(first_num, second_num):
-    
-    try:
-        return float(first_num) % float(second_num)
-    except ValueError as err:
-        result.config(text="ValueError")
-
-sum_bt = ttk.Button(root, text="+", command=lambda: result.config(text=calc_sum(first_num.get(), second_num.get())))
-sum_bt.pack()
-
-sub_bt = ttk.Button(root, text="-", command=lambda: result.config(text=calc_sub(first_num.get(), second_num.get())))
-sub_bt.pack()
-
-multi_bt = ttk.Button(root, text="*", command=lambda: result.config(text=calc_multi(first_num.get(), second_num.get())))
-multi_bt.pack()
-
-div_bt = ttk.Button(root, text="/", command=lambda: result.config(text=calc_div(first_num.get(), second_num.get())))
-div_bt.pack()
-
-exp_bt = ttk.Button(root, text="**", command=lambda: result.config(text=calc_exp(first_num.get(), second_num.get())))
-exp_bt.pack()
-
-mod_bt = ttk.Button(root, text="%", command=lambda: result.config(text=calc_mod(first_num.get(), second_num.get())))
-mod_bt.pack()
-
-
-root.mainloop()
+        try:
+            return float(first_num) % float(second_num)
+        except ValueError as err:
+            result.config(text="Unexpected value!")
