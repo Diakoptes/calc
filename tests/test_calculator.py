05c9669edc7ba06fa620ffa16d4ed66b4213d4e2
Lukasz Kasprzyk <l.kasprzyk@protonmail.com>
2022-04-21 21:56:44 +0200

    new tests with class Calculator

---
+++ tests/test_calculator.py
@@ -0,0 +1,29 @@
+from calculator import Calculator
+from layout import*
+import pytest
+
+def setup_module():
+    print('\n===Setup Module===')
+
+def test_sum():
+    assert Calculator.calc_sum(5, 5) == 10
+
+def test_sub():
+    assert Calculator.calc_sub(10, 5) == 5
+
+def test_multi():
+    assert Calculator.calc_multi(10, 2) == 20
+
+def test_div():
+    assert Calculator.calc_div(10, 1) == 10
+
+def test_exp():
+    assert Calculator.calc_exp(10, 2) == 100
+
+def test_mod():
+    assert Calculator.calc_mod(10, 3) == 1
+
+def teardown_module():
+    print('\n===Teardown Module===')
+
+
